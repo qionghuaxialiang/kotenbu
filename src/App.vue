c6ec31745aef19b0a688cb72093742f9c6f046f1
琼华夏凉 <fengkuang206@126.com>
2022-12-27 00:19:26 +0800

    实现前后端通信

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="欢迎使用TodoList！"/>
   </div>
 </template>
 
@@ -14,6 +14,10 @@ export default {
     HelloWorld
   }
 }
+
+
+
+
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,113 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>列表共有{{ list.length }}项事件，未完成{{item_undone}}项，已完成{{item_done}}项</h1>
+    <h1>    
+      <input v-model="message"/> 
+      <button @click="insert">提交</button>
+    </h1>
+
+
+    <h1 v-for="(item, index) of list" v-bind:key="index">{{ index+1 }}.{{ item.name }}:
+      
+      <li v-if="item.done!=0">已完成 
+        <button @click="update0(item.id)">设为未完成</button>
+      </li>
+        
+      <li v-else>未完成   
+        <button @click="update1(item.id)">设为已完成</button>
+      </li>
+    </h1>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    
+    return{
+      
+      message:'请输入代办内容',
+      list:[],
+      item_undone:0,
+      item_done:0
+
+    }
+  },
+  methods:{
+    getlist(){
+      axios({
+        method: 'get',
+      //url:'https://mock.apifox.cn/m1/2014704-0-default/todo/list',
+      url:'http://127.0.0.1:48080/getTodoList',
+      }).then((res) => {
+      //this.list = res.data.data.rows;
+      this.list = res.data;
+        console.log(this.list)
+      });
+    },
+
+    update0(input){
+      axios({
+        method: 'post',
+      url:'http://127.0.0.1:48080/update',
+      data:{
+        id: input,
+        done:0
+      }
+      }).then(function(response) {
+        console.log(response);
+      }).then(function(error){
+        console.log(error)
+      });
+      this.item_undone = this.item_undone + 1;
+      this.item_done = this.item_done - 1;
+    },
+    update1(input){
+      axios({
+        method: 'post',
+      url:'http://127.0.0.1:48080/update',
+      data:{
+        id:input,
+        done:1
+      }
+      }).then(function(response) {
+        console.log(response);
+      }).then(function(error){
+        console.log(error)
+      });
+      this.item_undone = this.item_undone - 1;
+      this.item_done = this.item_done + 1;
+    },
+    insert(){
+      axios({
+        method: 'post',
+      url:'http://127.0.0.1:48080/insert',
+      data:{
+        id:this.list.length,
+        name:this.message,
+        done:0
+      }
+      }).then(function(response) {
+        console.log(response);
+      }).then(function(error){
+        console.log(error)
+      });
+      this.message="";
+    },
+  
+  },
+  
+
+  created(){
+    this.getlist();
+
   }
 }
 </script>
